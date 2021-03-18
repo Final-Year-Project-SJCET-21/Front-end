78775616e37556c9027d19835a487a538081782b
justinjohnymathew <justinjohny.jj@gmail.com>
2021-03-18 15:05:29 +0530

    Initial Setup With Tailwind react and Redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage/homePage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="bg-white">
+    //   <header className="App-header bg-gray-500">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hey Justin
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <HomePage/>
   );
 }
 
